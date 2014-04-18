5bfe48d9c02d717d0ddaee98be69b605cc104546
Yusuke Higaki <yusuke.higaki@dzb.jp>
2014-04-18 18:44:36 +0900

    view DONE

---
+++ src/Acme/RentacarBundle/Controller/AppController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Acme\RentacarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * AppController.
+ *
+ * @author Yusuke Higaki <yusuke.higaki@dzb.jp>
+ */
+
+abstract class AppController extends Controller
+{
+}
+
+?>
